6e51af3fa21b2c6d61ec87221eb065e1f6c5a177
ManPaddy <onahfavour@gmail.com>
2023-03-21 10:54:52 +0100

    Task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -14,4 +14,13 @@ if __name__ == "__main__":
         print(row)
     cur.close()
     db.close()
+-- Create states table in hbtn_0e_0_usa with some data
+CREATE DATABASE IF NOT EXISTS hbtn_0e_0_usa;
+USE hbtn_0e_0_usa;
+CREATE TABLE IF NOT EXISTS states ( 
+	    id INT NOT NULL AUTO_INCREMENT, 
+	    name VARCHAR(256) NOT NULL,
+	    PRIMARY KEY (id)
+);
+INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
 
